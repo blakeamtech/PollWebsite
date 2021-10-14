c02e6c947ec9a13e3cd3410435d56f1ed5e0fc36
aCiotola <alessandromciotola@gmail.com>
2021-10-14 14:20:40 -0400

    Request Preparation
    Adds all methods needed to make the request for both user an poll manager

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * Class responsible for displaying and handling user poll requests.
+ */
+class App extends Component {
+    /**
+     * Constructor which allows functions to use the "this" keyword.     *
+     * @param props
+     */
+    constructor(props) {
+        super(props);
+        this.handleDownloadPoll = this.handleDownloadPoll.bind(this);
+        this.handleViewPoll = this.handleViewPoll.bind(this);
+    }
+
+    /***
+     * Function responsible for making a request to download the poll results to a text file.
+     */
+    handleDownloadPoll() {
+        axios.get('http://localhost:8080/details')
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    /**
+     * Function responsible for sending a request for poll results.
+     */
+    handleViewPoll() {
+        axios.get('http://localhost:8080/results')
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    /**
+     * Function responsible for sending a request when user votes in the poll.
+     */
+    handleVotePoll() {
+        var getData = null // Needs to be implemented
+        axios.post('http://localhost:8080/vote', getData)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    /***
+     * Function responsible for rendering tags for use in react methods.
+     * @returns {JSX.Element}
+     */
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <a href="pollmanager.xhtml">Go To Poll Manager</a><br/>
+                    <button type="button" onClick={this.handleViewPoll}>View Poll Results</button><br/>
+                    <button type="button" onClick={this.handleDownloadPoll}>Download Poll Results</button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
