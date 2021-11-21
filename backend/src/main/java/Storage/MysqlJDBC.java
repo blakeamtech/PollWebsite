2df00c4deaae4122f0b0e37a8f3b89d0e27757e6
CoricovMA <coricovacMA@gmail.com>
2021-11-21 13:08:30 -0500

    New requests (#35)
    * moved stuff around, started new requests
    
    * WIP, switching to db access instead of one poll instance
    
    * stuff
    
    * implemented vote
    
    * implemented authenticate
    
    * fixed poll jdbc insert and update

--- backend/src/main/java/Storage/MysqlJDBC.java
+++ backend/src/main/java/Storage/MysqlJDBC.java
@@ -21,12 +21,12 @@ public class MysqlJDBC {
     private static MysqlJDBC INSTANCE;
 
     private static final String INSERT_USER_QUERY = "INSERT INTO Users (name, email, password) values (?, ?, ?)";
-    private static final String INSERT_POLL_QUERY = "INSERT INTO Polls (pollId, title, question, email) values (?, ?, ?, ?)";
+    private static final String INSERT_POLL_QUERY = "INSERT INTO Polls (pollId, title, question, email, pollStatus) values (?, ?, ?, ?, ?)";
     private static final String INSERT_CHOICE_QUERY = "INSERT INTO Choices (pollId, choice) values (?, ?)";
     private static final String INSERT_VOTE_QUERY = "INSERT INTO Vote (PIN, choiceId) values (?, ?)";
 
     private static final String UPDATE_USER_QUERY = "UPDATE Users SET name = ?, email = ?, password = ? WHERE userId = ?";
-    private static final String UPDATE_POLL_QUERY = "Update Polls SET title = ?, question = ?, email = ? WHERE pollId = ?";
+    private static final String UPDATE_POLL_QUERY = "Update Polls SET title = ?, question = ?, email = ?, pollStatus = ? WHERE pollId = ?";
     private static final String UPDATE_CHOICE_QUERY = "UPDATE Choices SET pollId = ?, choice = ? WHERE choiceId = ?";
     private static final String UPDATE_VOTE_QUERY = "UPDATE Vote SET PIN = ?, choiceId = ? WHERE voteId = ?";
 
@@ -99,6 +99,7 @@ public class MysqlJDBC {
         statement.setString(2, poll.getPollTitle());
         statement.setString(3, poll.getQuestionText());
         statement.setString(4, poll.getEmail());
+        statement.setString(5, poll.getStatus().getValue());
         statement.executeUpdate();
         statement.close();
     }
@@ -214,6 +215,7 @@ public class MysqlJDBC {
         statement.setString(2, poll.getQuestionText());
         statement.setString(3, poll.getPollId());
         statement.setString(4, poll.getEmail());
+        statement.setString(5, poll.getStatus().getValue());
         statement.executeUpdate();
         statement.close();
     }
